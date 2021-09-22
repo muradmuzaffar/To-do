1b9afdd1d0de990542c6dcce17c75d189b6ffbb7
Murad <muzaffarov357@gmail.com>
2021-09-23 01:11:37 +0400

    The project is ready to use

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,67 @@
 <template>
-  <div id="app">
-    <h1 style="text-align:center;">To do List</h1>
-    <hr />
+  <div class="container mt-5" id="app">
+    <div class="row">
+      <h1 style="text-align:center;">To do List</h1>
+      <hr />
 
-    <!--to do input-->
-    <div class="input-group mb-3">
-      <input
-        v-model="myTodo.todo"
-        type="text"
-        class="form-control"
-        placeholder="Add to do"
-        aria-label="Recipient's username"
-        aria-describedby="basic-addon2"
-      />
-      <div class="input-group-append">
-        <button
-          @click="addTodo"
-          class="btn btn-outline-secondary"
-          type="button"
-        >
-          +
-        </button>
+      <!--to do input-->
+      <div class="input-group mb-3">
+        <input
+          v-model="myTodo.todo"
+          type="text"
+          class="form-control"
+          placeholder="Add to do"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <div class="input-group-append">
+          <button
+            @click="addTodo"
+            class="btn btn-outline-secondary"
+            type="button"
+          >
+            +
+          </button>
+        </div>
       </div>
-    </div>
 
-    <!--todos-->
-    <table class="table" v-if="myTodo.todos.length != 0">
-      <thead>
-        <tr>
-          <th scope="col">Index</th>
-          <th scope="col">Todo</th>
-          <th scope="col">Status</th>
-          <th scope="col">Delete</th>
-          <th scope="col">Update</th>
-        </tr>
-      </thead>
-      <tbody v-for="(todo, index) in myTodo.todos">
-        <tr>
-          <th scope="row">{{ index + 1 }}</th>
-          <td>{{ todo }}</td>
-          <td>
-            <input class="check" type="checkbox" />
-          </td>
-          <td>
-            <button @click="rv(index)" class="btn btn-danger btn-sm">
-              Delete
-            </button>
-          </td>
+      <!--todos-->
+      <table class="table" v-if="myTodo.todos.length != 0">
+        <thead>
+          <tr>
+            <th scope="col">Index</th>
+            <th scope="col">Todo</th>
+            <th scope="col">Status</th>
+            <th scope="col">Delete</th>
+            <th scope="col">Update</th>
+          </tr>
+        </thead>
+        <tbody v-for="(todo, index) in myTodo.todos">
+          <tr>
+            <th scope="row">{{ index + 1 }}</th>
+            <td>{{ todo }}</td>
+            <td>
+              <input class="check" type="checkbox" />
+            </td>
+            <td>
+              <button @click="rv(index)" class="btn btn-danger btn-sm">
+                Delete
+              </button>
+            </td>
 
-          <td v-if="updated == 0">
-            <button @click="up(index)" class="btn btn-success btn-sm">
-              Update
-            </button>
-          </td>
-          <td v-else="updated == 1">
-            <div class="input-group mb-3">
-              <input
-                v-model="myTodo.todo"
-                type="text"
-                class="form-control"
-                placeholder="Add to do"
-                aria-label="Recipient's username"
-                aria-describedby="basic-addon2"
-              />
-              <div class="input-group-append">
-                <button
-                  @click="addTodo"
-                  class="btn btn-outline-secondary"
-                  type="button"
-                >
-                  update
-                </button>
-              </div>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            <td>
+              <button @click="updateTodo(index)" class="btn btn-primary btn-sm">
+                Update
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
-    <!--alert-->
-    <div class="alert alert-danger" role="alert" v-else>
-      Please add a new task!
+      <!--alert-->
+      <div class="alert alert-danger" role="alert" v-else>
+        Please add a new task!
+      </div>
     </div>
   </div>
 </template>
@@ -93,19 +74,30 @@ export default {
         todo: "",
         todos: []
       },
-      updated: 0
+      editedTask: null
     };
   },
 
   methods: {
     addTodo: function() {
-      if (this.myTodo.todo.length != 0) {
+      if (this.myTodo.todo.length === 0) return;
+
+      if (this.editedTask != null) {
+        this.myTodo.todos[this.editedTask] = this.myTodo.todo;
+        this.editedTask = null;
+      } else {
         this.myTodo.todos.push(this.myTodo.todo);
         this.myTodo.todo = "";
       }
+
+      this.myTodo.todo = "";
     },
     rv: function(index) {
       this.myTodo.todos.splice(index, 1);
+    },
+    updateTodo: function(index) {
+      this.myTodo.todo = this.myTodo.todos[index];
+      this.editedTask = index;
     }
   }
 };
